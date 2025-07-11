ed0eb5fdf44e1c65bd583a6e0723c63ff54fa614
Cristian <crisemadiaz@gmail.com>
2025-07-11 02:03:43 +0000

    Primera versión dockerizada de mi app Python fullstack

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+# backend/app.py
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Habilita CORS para todas las rutas
+
+@app.route('/')
+def home():
+    return jsonify({"mensaje": "Hola desde el backend Flask!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
