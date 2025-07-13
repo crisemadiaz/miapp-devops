edbec818b602297079b623846e70df30b1854ba6
Cristian <crisemadiaz@gmail.com>
2025-07-13 02:30:15 +0000

    Se corrige archivo nginx.conf

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
 
-@app.route('/api')
 @app.route('/api/')
 def home():
     return jsonify({"mensaje": "Hola desde el backend Flask usando /api/"})
