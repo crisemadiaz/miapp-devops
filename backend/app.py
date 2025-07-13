dd94d6097a4a7045b328e7be033ff6d7486ca78d
Cristian <crisemadiaz@gmail.com>
2025-07-13 01:59:29 +0000

    Se agrega NGINX como proxy reverso

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
 
-@app.route('/')
+@app.route('/api/')
 def home():
-    return jsonify({"mensaje": "Hola desde el backend Flask!"})
+    return jsonify({"mensaje": "Hola desde el backend Flask usando /api/"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
