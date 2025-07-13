f892645933ffdd17fab40767753549a108633455
Cristian <crisemadiaz@gmail.com>
2025-07-13 02:19:34 +0000

    Se agregan rutas

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
 
+@app.route('/api')
 @app.route('/api/')
 def home():
     return jsonify({"mensaje": "Hola desde el backend Flask usando /api/"})
